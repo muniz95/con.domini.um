e2e8f53066733484202656b5737bb0bded25735a
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2020-07-24 00:15:51 -0300

    Small modifications

--- src/views/SmallAds/index.tsx
+++ src/views/SmallAds/index.tsx
@@ -7,7 +7,9 @@ const SmallAds: React.FC<{}> = observer(() => {
     <React.Fragment>
       <h2>Classificados</h2>
       <S.TabBar>
-        <S.Tab>Anúncios</S.Tab>
+        <S.Tab><button>Anúncios</button></S.Tab>
+        <S.Tab><button>Meus anúncios</button></S.Tab>
+        <S.Tab><button>Novo anúncio</button></S.Tab>
       </S.TabBar>
     </React.Fragment>
   );
--- src/views/SmallAds/styled.ts
+++ src/views/SmallAds/styled.ts
@@ -9,6 +9,16 @@ export const TabBar = styled.ul`
 export const Tab = styled.li`
   margin-top: -1px;
   border-style: solid;
+  list-style: none;
+  border: none;
+  display: list-item;
+  a {
+    color: #555;
+    cursor: default;
+    background-color: #fff;
+    border: 1px solid #ddd;
+    border-bottom-color: transparent;
+  }
 `;
 
 export default {
