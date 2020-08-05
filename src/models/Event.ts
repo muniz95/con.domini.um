caf31f09c6cea626ed6e4169fc332dee8057b186
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2020-08-05 13:33:04 -0300

    Events page done

---
+++ src/models/Event.ts
@@ -0,0 +1,27 @@
+class Event {
+  public static create(id: number, title: string, local: string, date: Date, attending?: boolean): Event {
+    const item = new Event();
+    item.id = id;
+    item.title = title;
+    item.title = local;
+    item.date = date;
+    item.attending = attending;
+    return item;
+  }
+
+  public id: number;
+  public title: string;
+  public local: string;
+  public date: Date;
+  public attending?: boolean;
+
+  constructor() {
+    this.id = 0;
+    this.title = "";
+    this.local = "";
+    this.date = new Date();
+    this.attending = undefined;
+  }
+}
+
+export default Event;
--- src/views/Events/index.tsx
+++ src/views/Events/index.tsx
@@ -1,8 +1,42 @@
 import { observer } from "mobx-react";
 import React from "react";
+import S from './styled';
+import EventsStore from './store';
 
 const Events: React.FC<{}> = observer(() => {
-  return <div>Eventos</div>;
+  const store = React.useContext(EventsStore);
+
+  React.useEffect(() => {
+    store.fetchItems();
+  }, [store]);
+  return (
+    <>
+      <h2>Eventos</h2>
+
+      <S.Center>
+        <S.Table>
+          <thead>
+            <tr>
+              <th>Título</th>
+              <th>Local</th>
+              <th>Data</th>
+              <th>Presença</th>
+            </tr>
+          </thead>
+          <tbody>
+          {store.events.map((event) =>
+            <tr key={event.id}>
+              <td>{event.title}</td>
+              <td>{event.local}</td>
+              <td>{event.date?.toLocaleDateString('pt-br')}</td>
+              <td>{event.attending ? 'Sim' : 'Não'}</td>
+            </tr>,
+          )}
+          </tbody>
+        </S.Table>
+      </S.Center>
+    </>
+  );
 });
 
 export default Events;
