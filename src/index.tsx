289faa18f8427ed88bdd78befc30e44caa18fcc7
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2025-03-29 01:41:35 -0300

    Integrate @tanstack/react-query for data fetching; wrap App component in QueryClientProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
+import { queryClient } from '@/lib/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/lib/react-query.ts
@@ -0,0 +1,34 @@
+import {
+  UseMutationOptions,
+  DefaultOptions,
+  QueryClient,
+} from '@tanstack/react-query';
+
+export const queryConfig = {
+  queries: {
+    refetchOnWindowFocus: false,
+    retry: false,
+    staleTime: 1000 * 60,
+  },
+} satisfies DefaultOptions;
+
+export type ApiFnReturnType<
+  FnType extends (...args: unknown[]) => Promise<unknown>,
+> = Awaited<ReturnType<FnType>>;
+
+export type QueryConfig<T extends (...args: unknown[]) => unknown> = Omit<
+  ReturnType<T>,
+  'queryKey' | 'queryFn'
+>;
+
+export type MutationConfig<
+  MutationFnType extends (...args: unknown[]) => Promise<unknown>,
+> = UseMutationOptions<
+  ApiFnReturnType<MutationFnType>,
+  Error,
+  Parameters<MutationFnType>[0]
+>;
+
+export const queryClient = new QueryClient({
+  defaultOptions: queryConfig,
+});
