ff95e3894e0f62b44423165d08b847ea7ed1907c
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2025-03-29 20:30:27 -0300

    Refactor Album and Dweller models: remove constructors and static create method for cleaner interface definition

--- src/models/Album.ts
+++ src/models/Album.ts
@@ -1,29 +1,8 @@
 interface Album {
-  static create(
-    id: number,
-    title: string,
-    cover: string,
-    date: Date
-  ): Album {
-    const item = new Album();
-    item.id = id;
-    item.title = title;
-    item.cover = cover;
-    item.date = date;
-    return item;
-  }
-
   id: number;
   title: string;
   cover: string;
   date: Date;
-
-  constructor() {
-    this.id = 0;
-    this.title = '';
-    this.cover = '';
-    this.date = new Date();
-  }
 }
 
 export default Album;
--- src/models/Dweller.ts
+++ src/models/Dweller.ts
@@ -6,26 +6,6 @@ interface Dweller {
   birthdate: Date;
   mobilityRestriction: boolean;
   userId: number;
-
-  constructor(params: any) {
-    this.id = params.id;
-    this.name = params.name;
-    this.rg = params.rg;
-    this.kinship = params.kinship;
-    this.birthdate = new Date(params.birthdate);
-    this.mobilityRestriction = params.mobilityRestriction;
-    this.userId = params.userId;
-  }
-
-  get age() {
-    const ageDifMs = Date.now() - this.birthdate.getTime();
-    const ageDate = new Date(ageDifMs);
-    return Math.abs(ageDate.getUTCFullYear() - 1970).toString();
-  }
-
-  get hasMobilityRestriction() {
-    return this.mobilityRestriction ? 'Sim' : 'Não';
-  }
 }
 
 export default Dweller;
