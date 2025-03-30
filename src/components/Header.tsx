b562eab8cbdf8180f2c4db4368db69250e161fbc
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2025-03-30 20:37:18 -0300

    Refactor Header component: remove unused useNavigate import

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ import ListItemText from '@mui/material/ListItemText';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 interface IMenuEntry {
   name: string;
